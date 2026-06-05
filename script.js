93c3448392f8af66ad1a8ca2348635fd9f79beea
marvigarg <marvigarg181@gmail.com>
2026-06-05 00:40:49 -0400

    home screen redesign

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('getStartedBtn').addEventListener('click', function() {
+  document.getElementById('homeScreen').style.display = 'none'
+  document.getElementById('onboardingScreen').style.display = 'block'
+})
