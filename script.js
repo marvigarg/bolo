d0fee71386950ac803015217033436d516079875
marvigarg <marvigarg181@gmail.com>
2026-06-13 21:27:08 -0400

    name screen done

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ document.getElementById('getStartedBtn').addEventListener('click', function() {
   document.getElementById('homeScreen').style.display = 'none'
   document.getElementById('whoScreen').style.display = 'flex'
 })
+
+document.getElementById('myselfBtn').addEventListener('click', function() {
+  document.getElementById('whoScreen').style.display = 'none'
+  document.getElementById('nameLabel').textContent = 'What is your name?'
+  document.getElementById('nameInput').placeholder = 'Enter your name'
+  document.getElementById('nameScreen').style.display = 'flex'
+})
+
+document.getElementById('someoneBtn').addEventListener('click', function() {
+  document.getElementById('whoScreen').style.display = 'none'
+  document.getElementById('nameLabel').textContent = 'What is your patient\'s name?'
+  document.getElementById('nameInput').placeholder = 'Enter patient\'s name'
+  document.getElementById('nameScreen').style.display = 'flex'
+})
